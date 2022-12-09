2dceed76399a690b36134f62bc2aeb0ae0e7b0d2
Quentin RIDEL <quentin.ridel@etu.univ-rouen.fr>
2022-12-09 16:47:28 +0100

    init: first commit

---
+++ src/test/java/com/univrouen/springsecuritytest/SpringSecurityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univrouen.springsecuritytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
